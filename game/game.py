576d740cd839f653ad4439687bceadc8b552f55f
TristanCl <91383479+TristanCl@users.noreply.github.com>
2021-10-02 18:26:22 +0200

    Add files via upload
    changing game to match the new things to display

--- game/game.py
+++ game/game.py
@@ -3,7 +3,8 @@ import sys
 from .map import Map
 from settings import *
 import os
-
+from player import *
+from villager import *
 
 
 class Game:
@@ -12,6 +13,16 @@ class Game:
         self.clock = clock
         self.width, self.height = self.screen.get_size()
         self.map = Map(10,10, self.width, self.height)
+
+        # Writing style
+        myfont = pygame.font.SysFont("monospace", 20)
+
+        #  INIT FOR RESSOURCES DISPLAY
+        self.display_stone = myfont.render(str(playerOne.ressources[0]), True, (10, 10, 10))
+        self.display_gold = myfont.render(str(playerOne.ressources[1]), True, (10, 10, 10))
+        self.display_lumber = myfont.render(str(playerOne.ressources[2]), True, (10, 10, 10))
+        self.display_food = myfont.render(str(playerOne.ressources[3]), True, (10, 10, 10))
+
 
     def run(self):
         self.playing = True
@@ -22,29 +33,46 @@ class Game:
             self.draw()
 
     def events(self):
-        #quit game
+
         for event in pygame.event.get():
+
+            # quit game
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+
+            # USER PRESSED A KEY
             if event.type == pygame.KEYDOWN:
+
                 if event.key == pygame.K_ESCAPE:
                     # Quit to menu screen instead maybe
                     pygame.quit()
                     sys.exit()
 
+            # USER PRESSED A MOUSEBUTTON
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+
+                if event.button == 1:  #  LEFT CLICK
+                    ...
+                elif event.button == 3:  # RIGHT CLICK
+                    if testUnit1.is_alive == True:
+                        testUnit2.attack(testUnit1)
     def update(self):
         # Build later
         pass
 
+    # GAME DISPLAY
     def draw(self):
+
+        # BLACK BACKGROUND
         self.screen.fill((0,0,0))
 
+        # FOR THE MAP
         for x in range(self.map.grid_length_x):
             for y in range(self.map.grid_length_y):
                 square = self.map.map[x][y]["drect"]
                 rect = pygame.Rect(square[0][0], square[0][1], TILE_SIZE,TILE_SIZE)
-                pygame.draw.rect(self.screen, (0,255,255),rect,1)
+                #pygame.draw.rect(self.screen, (0,255,255),rect,1)
                 #pygame.draw.rect(surface, color, rect, width...)
 
                 render_pos = self.map.map[x][y]["render_pos"]
@@ -52,8 +80,35 @@ class Game:
 
                 p = self.map.map[x][y]["iso_poly"]
                 p = [(x+self.width/2, y + self.height/4) for x, y in p]
-                pygame.draw.polygon(self.screen, (255,255,255), p, 1)
+                #pygame.draw.polygon(self.screen, (255,255,255), p, 1)
 
+        # ressources display
+        self.screen.blit(top_menu, (180, 95))
+        self.screen.blit(self.display_stone, (300, 130))
+        self.screen.blit(self.display_gold, (500, 130))
+        self.screen.blit(self.display_lumber, (700, 130))
+        self.screen.blit(self.display_food, (900, 130))
+
+        # units display
+        for a_unit in units_group:
+            if (a_unit.is_alive):
+                a_unit.display(self.screen)
+
+                #health bar display
+                a_unit.display_life(self.screen)
+
+        # buildings display
+        ...
+
+        # MOUSE CURSOR - we disable the default one and create a new one at the current position of the mouse
+        # MUST BE LAST TO SEE IT AND NOT BE HIDDEN BEHIND OTHER THINGS
+        pygame.mouse.set_visible(False)
+        standard_cursor_rect = standard_cursor.get_rect()
+        standard_cursor_rect.center = pygame.mouse.get_pos()  # update position
+        self.screen.blit(standard_cursor, standard_cursor_rect)  # draw the cursor
+
+        # to refresh the screen and display things properly
         pygame.display.flip()
+
 
 
