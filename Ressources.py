8ee202e809e43fb68664fcb27d49645b71f1cb8f
DrewSam <64272265+DrewSam@users.noreply.github.com>
2021-10-03 19:16:39 +0200

    Create Ressources.py

---
+++ Ressources.py
@@ -0,0 +1,26 @@
+import pygame
+pygame.init()
+
+#créer une classe qui représente les ressources
+
+class Ressource:
+  def  __init__(self):
+    
+    self.nbRessources = 0
+    self.typeRessource = ""
+    
+  def __init__(self, nbRess, laRess):
+    self.nbRessources = nbRess
+    self.typeRessource = laRess
+
+  def getTypeRessource(self):
+    return self.typeRessource
+
+  def getNbRessources(self):
+    return self.nbRessources
+
+  def setTypeRessource(self, typeRess):
+    self.typeRessource = typeRess
+   
+  def setNbRessources(self, nbRess):
+    self.nbRessources = nbRess
