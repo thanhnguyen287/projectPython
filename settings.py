27ebb770578a0c4553b75e35bd08a5cbfd14ef7c
Tristan <91383479+TristanCl@users.noreply.github.com>
2021-10-10 13:53:43 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -14,8 +14,8 @@ current_path = os.path.dirname(__file__) #current path of settings.py
 resource_path = os.path.join(current_path, 'resources') 
 common_path = os.path.join(resource_path, 'common')
 assets_path = os.path.join(resource_path, 'assets')
-top_menu = pygame.image.load ("resources/assets/top_menu.png").convert_alphas()
-standard_cursor = pygame.image.load ("resources/assets/standard_cursor.png").convert_alpha()
+top_menu = pygame.image.load ("resources/assets/top_menu.png")
+standard_cursor = pygame.image.load ("resources/assets/standard_cursor.png")
 
 myfont = pygame.font.SysFont("monospace", 20)
 
