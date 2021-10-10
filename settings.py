12029781ebccb0f1ed87e176e3b279e59b5a400a
Tristan <91383479+TristanCl@users.noreply.github.com>
2021-10-10 13:46:42 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,22 +1,21 @@
 import os
 import pygame
+
 TILE_SIZE = 64
 WIDTH = 1920
 HEIGHT = 1080
 
 ENABLE_HEALTH_BARS = False
 
+pygame.font.init()
+
 #path
 current_path = os.path.dirname(__file__) #current path of settings.py
 resource_path = os.path.join(current_path, 'resources') 
 common_path = os.path.join(resource_path, 'common')
 assets_path = os.path.join(resource_path, 'assets')
-top_menu = pygame.image.load ("resources/assets/top_menu.png")
-standard_cursor = pygame.image.load ("resources/assets/standard_cursor.png")
+top_menu = pygame.image.load ("resources/assets/top_menu.png").convert_alphas()
+standard_cursor = pygame.image.load ("resources/assets/standard_cursor.png").convert_alpha()
 
-def decarte_to_iso(x, y):
-    iso_x = x - y
-    iso_y = (x + y) / 2
-    return iso_x, iso_y
-
+myfont = pygame.font.SysFont("monospace", 20)
 
